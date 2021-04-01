3cfadf81c17a33d25c78f8f23dff5ac6f001e54a
YOANN AUDOUIN <B61570@dsp0919998.atlas.edf.fr>
2021-04-01 10:03:03 +0200

    [test] Adding test for ElectromagnetismStreamTraceur

---
+++ src/ElectromagnetismStreamTraceur/Test/test_maxwell_StreamLines_0.py
@@ -0,0 +1,44 @@
+from paraview.simple import *
+from medcoupling import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+def MyAssert(clue):
+    if not clue:
+        raise RuntimeError("Assertion failed !")
+
+fname = "maxwell_streamline.med"
+
+arr = DataArrayDouble([0,1,2,3,4,5,6,7,8,9,10])
+m = MEDCouplingCMesh()
+m.setCoords(arr,arr,arr)
+m = m.buildUnstructured()
+
+m.changeSpaceDimension(3,0.)
+m.setName("mesh")
+f = MEDCouplingFieldDouble(ON_CELLS)
+f.setMesh(m)
+f.setName("field")
+arrf = DataArrayDouble(10*10*10,3)
+arrf[:,0] = 1 ; arrf[:,1] = 0 ; arrf[:,2] = 0
+f.setArray( arrf )
+f.getArray().setInfoOnComponents(["X","Y","Z"])
+f.checkConsistencyLight()
+f.write(fname)
+f2 = f.deepCopy()
+arrf2 = DataArrayDouble(10*10*10,3)
+arrf2[:,0] = 0 ; arrf2[:,1] = 1 ; arrf2[:,2] = 0
+f2.setArray( arrf2 )
+f2.setName("field2")
+WriteFieldUsingAlreadyWrittenMesh(fname,f2)
+
+testmed = MEDReader(FileName=fname)
+testmed.AllArrays = ['TS0/mesh/ComSup0/field@@][@@P0', 'TS0/mesh/ComSup0/field2@@][@@P0', 'TS0/mesh/ComSup0/mesh@@][@@P0']
+testmed.AllTimeSteps = ['0000']
+streamTraceur1 = LigneDeChamp(Input=testmed,SeedType='Point Cloud')
+streamTraceur1.SeedType.Radius = 1
+streamTraceur1.SeedType.Center = [ 7.23,7.26,3.42 ]
+streamTraceur1.Vectors = ['CELLS', "field"]
+streamTraceur1.UpdatePipeline()
+ds0 = servermanager.Fetch(streamTraceur1)
+MyAssert(ds0.GetNumberOfCells()==200)
